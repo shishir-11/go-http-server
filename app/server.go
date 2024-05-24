83ebecab56d0b214067971d107710237d9524704
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-05-24 16:00:29 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net/http"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
+func HandleConn(conn net.Conn) {
+	defer conn.Close()
+	req, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		log.Println("Error reading request")
+	}
+	// log.Println(buf)
+	// if strings.HasPrefix(stri)
+	if req.URL.Path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -24,6 +42,5 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	HandleConn(connection)
 }
